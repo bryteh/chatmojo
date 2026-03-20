54cb0de933fe138b48698b403ddf07a2e7ca5211
Bryan <davincimj@gmail.com>
2026-03-21 01:14:24 +0800

    fix(whatsapp): preserve nested processed_params by selectively allowing permit! to bypass rails flat-hash security constraint

--- app/controllers/api/v1/accounts/campaigns_controller.rb
+++ app/controllers/api/v1/accounts/campaigns_controller.rb
@@ -28,7 +28,9 @@ class Api::V1::Accounts::CampaignsController < Api::V1::Accounts::BaseController
   end
 
   def campaign_params
-    params.require(:campaign).permit(:title, :description, :message, :enabled, :trigger_only_during_business_hours, :inbox_id, :sender_id,
-                                     :scheduled_at, audience: [:type, :id], trigger_rules: {}, template_params: {})
+    permitted = params.require(:campaign).permit(:title, :description, :message, :enabled, :trigger_only_during_business_hours, 
+                                                 :inbox_id, :sender_id, :scheduled_at, audience: [:type, :id], trigger_rules: {})
+    permitted[:template_params] = params[:campaign][:template_params].permit! if params[:campaign][:template_params].present?
+    permitted
   end
 end
